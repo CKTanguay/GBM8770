367ec199ec60776b55d3f5f304edc0891d427d6d
Valerie Patenaude <vapatb@gigl.polymtl.ca>
2019-09-16 12:04:42 -0400

    Tentative d'avancer dans le lab

--- TP1/TP.m
+++ TP1/TP.m
@@ -1,7 +1,10 @@
 %% Exercice 1
 % Q1
-imread ("fundus_image.png")
-imshow("fundus_image.png")
+matrix=imread("fundus_image.png");
+%imshow("fundus_image.png")
 
 % Q2
+f=2;
+image_reduite = reduire(matrix, f);
+imshow(image_reduite)
 
